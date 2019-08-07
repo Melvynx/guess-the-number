8db147f5023cdde58b43c62a5407f03a4a45915c
Melvyn <melvyn.malherbe@qoqa.com>
2019-08-07 17:19:50 +0200

    Refactor

--- code.js
+++ code.js
@@ -1,19 +1,9 @@
-const buttonStart = document.getElementById('start_game')
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
 let randomNumber = 0;
 let tryCount = 0; //compteur d'essai
 let game_start = false; //si la game à commencer
-let infoStart = document.getElementById('infoStart');
-//début de partie
+const infoStart = document.getElementById('infoStart');
 
-let getHighScore = function () {
-  localHighScore = JSON.parse(localStorage.getItem("high_score"));
-  return localHighScore;
-}
-getHighScore();
-
+const buttonStart = document.getElementById('start_game');
 buttonStart.onclick = function () {
   randomNumber = getRandomInt(100);
   console.log("Game started with number:", randomNumber);
@@ -24,69 +14,71 @@ buttonStart.onclick = function () {
   answer.innerHTML = "";
 }
 
+// Utils
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getHighScore() {
+  return JSON.parse(localStorage.getItem("high_score"));
+}
+
 const sendNumber = document.getElementById('sendNumber');
-let numberByUser = 0;
-numberByUser = document.getElementById('number_give');
-
-let high_score = ["Melvyn;12", "Michel;7"];
-
-
+const numberByUser = document.getElementById('number_give');
 
 //lorsque qu'on clic sur tester
 sendNumber.onclick = function () {
-  
-  let answer = document.getElementById('answer');
+
+  const answer = document.getElementById('answer');
   newNumber = Number(numberByUser.value);
   numberByUser.value = "";
 
-  console.log("User have send the number: ", newNumber);
+  console.log("User have sent the number: ", newNumber);
+
 
   tryCount++;
-  console.log(tryCount);
-  if (game_start === true) {
+  if (game_start) {
     if (randomNumber === newNumber) {
       answer.innerHTML = `<h2><span class="h2_color1">B</span><span class="h2_color2">R</span><span class="h2_color3">A</span><span class="h2_color4">V</span><span class="h2_color5">O</span></h2>
       Tu as réussi à trouver le nombre que je pensais qui était <span class="configuration_number">${randomNumber}</span>. <br/> Tu l'as trouvé en ${tryCount} essais. `;
-      
-      console.log("Game stop successful");
+
       game_start = false;
-      let pseudoU = prompt('Donnez moi votre pseudo :');
-      high_score.push(`${pseudoU};${tryCount}`);
-      localStorage.setItem("high_score", JSON.stringify(high_score))
-      console.log(high_score);
+            console.log("Game stop successful");
 
+      const pseudoUser = prompt('Donnez moi votre pseudo :');
+      const previousHighScore = localStorage.getItem('high_score');
+      let highScore;
 
+      if (previousHighScore) {
+        highScore = JSON.parse(previousHighScore);
+        highScore.push(`${pseudoUser};${tryCount}`);
+      } else {
+        highScore = [`${pseudoUser};${tryCount}`];
+      }
 
-
-
-
-
-
-
-
-
+      localStorage.setItem("high_score", JSON.stringify(highScore))
 
 
     } else if (newNumber < 0) {
       answer.innerHTML = "Tu dois donner un nombre positif.";
-      setTimeout(function(){ answer.innerHTML = ""; }, 2200)
+      setTimeout(function () { answer.innerHTML = ""; }, 2200)
     } else if (newNumber > 100) {
       answer.innerHTML = `Si je te dis un nombre entre 0 et 100 c'est pas pour me donner ${newNumber} ! Ta bus quoi ??`;
     }
     else if (randomNumber > newNumber) {
       answer.innerHTML = "Ton nombre est trop petit !!";
-      setTimeout(function(){ answer.innerHTML = ""; }, 2200)
+      setTimeout(function () { answer.innerHTML = ""; }, 2200)
     } else if (randomNumber < newNumber) {
       answer.innerHTML = "Ton nombre est trop grand !!";
-      setTimeout(function(){ answer.innerHTML = ""; }, 2200)
-    } 
+      setTimeout(function () { answer.innerHTML = ""; }, 2200)
+    }
     else {
       answer.innerHTML = "Ton nombre n'en ai pas un.";
-      setTimeout(function(){ answer.innerHTML = ""; }, 2200)
+      setTimeout(function () { answer.innerHTML = ""; }, 2200)
     }
-  }else {
+  } else {
     answer.innerHTML = "La partie n'a pas encore commencer.";
-    setTimeout(function(){ answer.innerHTML = ""; }, 3000)
+    setTimeout(function () { answer.innerHTML = ""; }, 3000)
   }
 
 }
@@ -103,24 +95,11 @@ buttonStop.onclick = function () {
   infoStart.innerHTML = "Appuis sur start pour commencer la partie<br />Bonne chance :)";
 }
 
-
-function splitString(stringToSplit, separator) {
-  return stringToSplit.split(separator);
-}
-
+const localHighScore = getHighScore();
 for (let i = 0; i < localHighScore.length; i++) {
-  const array = splitString(localHighScore[i], ";");
+  const array = localHighScore[i].split(";")
   let pHighScore = document.getElementById(`high_score${i}`);
 
   console.log(pHighScore)
   pHighScore.innerHTML = `${i + 1}. ${array[0]} en ${array[1]} essais.`;
-
 }
-
-
-
-
-
-
-
-
