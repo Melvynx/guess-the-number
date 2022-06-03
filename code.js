93f2dfe79fc1972f672265bf2d7ed7e8c1a9a22e
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-06-03 10:18:27 +0200

    add gh-pages

--- code.js
+++ code.js
@@ -5,30 +5,34 @@ const infoStart = document.getElementById('infoStart');
 const localHighScore = getHighScore();
 const buttonStart = document.getElementById('start_game');
 const titleHighScore = document.getElementById('titleHighScore');
-const h1 = document.getElementById("h1");
+const h1 = document.getElementById('h1');
+
 buttonStart.onclick = function () {
   if (gameStart === false) {
     randomNumber = getRandomInt(100);
-    console.log("Game started with number:", randomNumber);
+    console.log('Game started with number:', randomNumber);
     gameStart = true;
-    infoStart.innerHTML = "La partie à commencer. <br /> Tu peux remplir le champs avec un nombre positif.";
-    buttonStart.innerHTML = "Restart";
+    infoStart.innerHTML =
+      'La partie à commencer. <br /> Tu peux remplir le champs avec un nombre positif.';
+    buttonStart.innerHTML = 'Restart';
     tryCount = 0;
-    answer.innerHTML = "";
+    answer.innerHTML = '';
   } else {
     randomNumber = getRandomInt(100);
-    console.log("Game started with number:", randomNumber);
+    console.log('Game started with number:', randomNumber);
     tryCount = 0;
-    answer.innerHTML = "";
-    infoStart.innerHTML = 'La partie de <span id="infosrart_re">recommencer</span>. Le nombre à été regénéré. <br /> Tu peux remplir le champs avec un nombre positif.';
-    setTimeout(function () { answer.innerHTML = ""; }, 2200);
+    answer.innerHTML = '';
+    infoStart.innerHTML =
+      'La partie de <span id="infosrart_re">recommencer</span>. Le nombre à été regénéré. <br /> Tu peux remplir le champs avec un nombre positif.';
     setTimeout(function () {
-      infoStart.innerHTML = "La partie à commencer. <br /> Tu peux remplir le champs avec un nombre positif.";
-      
+      answer.innerHTML = '';
+    }, 2200);
+    setTimeout(function () {
+      infoStart.innerHTML =
+        'La partie à commencer. <br /> Tu peux remplir le champs avec un nombre positif.';
     }, 3500);
-
   }
-}
+};
 
 // Utils
 function getRandomInt(max) {
@@ -36,15 +40,17 @@ function getRandomInt(max) {
 }
 
 function getHighScore() {
-  return JSON.parse(localStorage.getItem("high_score"));
+  return JSON.parse(localStorage.getItem('high_score'));
 }
 
 function getUserPseudo() {
-  const pseudo = prompt(`Bravo ! Tu as réussi en ${tryCount} essai. Donne moi ton pseudo pour faire partie du highScore !`);
+  const pseudo = prompt(
+    `Bravo ! Tu as réussi en ${tryCount} essai. Donne moi ton pseudo pour faire partie du highScore !`
+  );
   if (pseudo === null) {
     return null;
   }
-  if (pseudo === "") {
+  if (pseudo === '') {
     return null;
   }
   if (pseudo === undefined) {
@@ -58,14 +64,13 @@ function getUserPseudo() {
     if (localHighScore === null) {
       return pseudo;
     } else {
-
-      const found = localHighScore.find(highScore => highScore.pseudo === pseudo);
+      const found = localHighScore.find((highScore) => highScore.pseudo === pseudo);
 
       if (found) {
         alert('Votre pseudo est déjà utilisé.');
         return getUserPseudo();
       }
-      return pseudo
+      return pseudo;
     }
   }
 }
@@ -74,25 +79,16 @@ function changeStyleAnswer(couleur, fontWeight) {
   answer.style.fontWeight = fontWeight;
 }
 
-
-
-
-
-
-
 const sendNumber = document.getElementById('sendNumber');
 const numberByUser = document.getElementById('number_give');
 
-
 //lorsque qu'on clic sur tester
 sendNumber.onclick = function () {
-
   const answer = document.getElementById('answer');
   newNumber = Number(numberByUser.value);
-  numberByUser.value = "";
+  numberByUser.value = '';
 
-  console.log("User have sent the number: ", newNumber);
-
+  console.log('User have sent the number: ', newNumber);
 
   tryCount++;
   if (gameStart) {
@@ -101,14 +97,13 @@ sendNumber.onclick = function () {
       Tu as réussi à trouver le nombre que je pensais qui était <span class="configuration_number">${randomNumber}</span>. <br/> Tu l'as trouvé en ${tryCount} essais. `;
 
       gameStart = false;
-      console.log("Game stop successful");
+      console.log('Game stop successful');
 
       pseudoUser = getUserPseudo();
       const previousHighScore = localStorage.getItem('high_score');
       let highScore;
 
       if (pseudoUser === null) {
-
       } else {
         if (previousHighScore) {
           highScore = JSON.parse(previousHighScore);
@@ -118,78 +113,118 @@ sendNumber.onclick = function () {
         }
       }
 
-      localStorage.setItem("high_score", JSON.stringify(highScore))
+      localStorage.setItem('high_score', JSON.stringify(highScore));
       //let b = [{ pseudo: "Pascal", score: 13 }, { pseudo: "Lopiro", score: 11 }, { pseudo: "Calme", score: 25 }, { pseudo: "sorcière", score: 20 }, { pseudo: "PascalLeGrandFrere", score: 22 }, { pseudo: "Jean-mi", score: 65 }, { pseudo: "Aurelien", score: 16 }, { pseudo: "LeMenuisier", score: 30 },{ pseudo: "Karott", score: 85 },{ pseudo: "Zèbre", score: 38 }, { pseudo: "Pommier", score: 56 }, { pseudo: "PatateOfourt", score: 58 }, { pseudo: "Toto", score: 89 }];
 
       window.location.reload();
     } else if (newNumber < 0) {
-      answer.innerHTML = newNumber + ": Tu dois donner un nombre positif.";
-      changeStyleAnswer("black", "bold");
-      console.log(answer.style.fontWeight)
-      setTimeout(function () { answer.innerHTML = ""; }, 2200)
+      answer.innerHTML = newNumber + ': Tu dois donner un nombre positif.';
+      changeStyleAnswer('black', 'bold');
+      console.log(answer.style.fontWeight);
+      setTimeout(function () {
+        answer.innerHTML = '';
+      }, 2200);
     } else if (newNumber > 100) {
       answer.innerHTML = `Si je te dis un nombre entre 0 et 100 c'est pas pour me donner ${newNumber} ! Ta bus quoi ??`;
-      changeStyleAnswer("black", "normal");
-      setTimeout(function () { answer.innerHTML = ""; }, 3500)
-    }
-
-    else if (randomNumber > newNumber) {
-      answer.innerHTML = newNumber + ": Ce nombre est trop petit !!";
-      setTimeout(function () { answer.innerHTML = ""; }, 2200)
-      changeStyleAnswer("#05308b", "normal");
+      changeStyleAnswer('black', 'normal');
+      setTimeout(function () {
+        answer.innerHTML = '';
+      }, 3500);
+    } else if (randomNumber > newNumber) {
+      answer.innerHTML = newNumber + ': Ce nombre est trop petit !!';
+      setTimeout(function () {
+        answer.innerHTML = '';
+      }, 2200);
+      changeStyleAnswer('#05308b', 'normal');
     } else if (randomNumber < newNumber) {
-      answer.innerHTML = newNumber + ": Ce nombre est trop grand !!";
-      setTimeout(function () { answer.innerHTML = ""; }, 2200)
-      changeStyleAnswer("#7a1010", "bold");
-    }
-    else {
+      answer.innerHTML = newNumber + ': Ce nombre est trop grand !!';
+      setTimeout(function () {
+        answer.innerHTML = '';
+      }, 2200);
+      changeStyleAnswer('#7a1010', 'bold');
+    } else {
       answer.innerHTML = "Ce nombre n'en ai pas un.";
-      setTimeout(function () { answer.innerHTML = ""; }, 2200)
-      changeStyleAnswer("#592d01", "normal");
+      setTimeout(function () {
+        answer.innerHTML = '';
+      }, 2200);
+      changeStyleAnswer('#592d01', 'normal');
     }
   } else {
     answer.innerHTML = "Erreur: La partie n'a pas encore commencer.";
-    setTimeout(function () { answer.innerHTML = ""; }, 3000)
-    changeStyleAnswer("#592d01", "normal");
+    setTimeout(function () {
+      answer.innerHTML = '';
+    }, 3000);
+    changeStyleAnswer('#592d01', 'normal');
   }
-
-}
+};
 
 const buttonStop = document.getElementById('game_stop');
 
 buttonStop.onclick = function () {
-  randomNumber = "";
+  randomNumber = '';
   gameStart = false;
-  infoStart.innerHTML = "";
-  buttonStart.innerHTML = "Start";
-  console.log("Game stop successful");
+  infoStart.innerHTML = '';
+  buttonStart.innerHTML = 'Start';
+  console.log('Game stop successful');
   tryCount = 0;
-  infoStart.innerHTML = "Appuis sur start pour commencer la partie<br />Bonne chance :)";
-}
-
-
-
-
+  infoStart.innerHTML =
+    'Appuis sur start pour commencer la partie<br />Bonne chance :)';
+};
 
 // const localHighScore = getHighScore(); je vais ligne 5 moi a plus
-const localHighScoreBest = localHighScore.sort((a, b) => (a.score > b.score) ? 1 : ((b.score > a.score) ? -1 : 0));
+const localHighScoreBest = localHighScore.sort((a, b) =>
+  a.score > b.score ? 1 : b.score > a.score ? -1 : 0
+);
 for (let i = 0; i < localHighScoreBest.length; i++) {
   if (i < 10) {
     let pHighScore = document.getElementById(`high_score${i}`);
 
-    console.log(pHighScore)
-    pHighScore.innerHTML = `${i + 1}. ${localHighScoreBest[i].pseudo} en ${localHighScoreBest[i].score} essais.`;
+    console.log(pHighScore);
+    pHighScore.innerHTML = `${i + 1}. ${localHighScoreBest[i].pseudo} en ${
+      localHighScoreBest[i].score
+    } essais.`;
   } else {
-    console.log("more up");
+    console.log('more up');
   }
 }
-//Pour changer le tableau : mettre le nouveau tableau dans une variable (a = [1,2,3,4]) puis l'égaliser avec le tableau enregistrer (highScore = a), et set le tableau dans le local storage 
+//Pour changer le tableau : mettre le nouveau tableau dans une variable (a = [1,2,3,4]) puis l'égaliser avec le tableau enregistrer (highScore = a), et set le tableau dans le local storage
 // localStorage.setItem("high_score", JSON.stringify(highScore))
 
-const color = ["blue", "red", "yellow", "rosybrown", "royalblue", "hotpink", "lawngreen", "white", "aqua", "purple", "olivedrab", "orange", "yellowgreen", "maroon", "pink",];
-const textHighScore = ["MeilLeur Sco_re", "FeiLLeur Score", "Meill3ur 5cor3", "M3illeur Sc0re", "MeilleUR Score", "SCORE MeiLLeur", "m33lleur ScoRE", "MeiooeuR sc0re", "M$éll$eur ScOrE", "M3IkLEUR Score", "Meilleur SCORE", "meilleur Sc0RE", "Meill__r Sc_re", "M!ill!ur Score", "M!il__ur Sc!re", ];
-const colorh1 = ["#592d01", "#301d0b", "#924d0d",]
-
+const color = [
+  'blue',
+  'red',
+  'yellow',
+  'rosybrown',
+  'royalblue',
+  'hotpink',
+  'lawngreen',
+  'white',
+  'aqua',
+  'purple',
+  'olivedrab',
+  'orange',
+  'yellowgreen',
+  'maroon',
+  'pink',
+];
+const textHighScore = [
+  'MeilLeur Sco_re',
+  'FeiLLeur Score',
+  'Meill3ur 5cor3',
+  'M3illeur Sc0re',
+  'MeilleUR Score',
+  'SCORE MeiLLeur',
+  'm33lleur ScoRE',
+  'MeiooeuR sc0re',
+  'M$éll$eur ScOrE',
+  'M3IkLEUR Score',
+  'Meilleur SCORE',
+  'meilleur Sc0RE',
+  'Meill__r Sc_re',
+  'M!ill!ur Score',
+  'M!il__ur Sc!re',
+];
+const colorh1 = ['#592d01', '#301d0b', '#924d0d'];
 
 function changeColorh1(counth1) {
   setTimeout(() => {
@@ -201,14 +236,13 @@ function changeColorh1(counth1) {
 changeColorh1(0);
 
 function changeColor(countColor) {
-  setTimeout(function () { 
+  setTimeout(function () {
     titleHighScore.style.backgroundColor = color[countColor];
     titleHighScore.innerHTML = textHighScore[countColor];
     const newValue = (countColor + 1) % color.length;
     changeColor(newValue);
   }, 2500);
 }
-changeColor(0)
+changeColor(0);
 
 //setTimeout(function () { titleHighScore.style.backgroundColor = color[8] }, 3500);
-
